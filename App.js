6f05ac57d80486bd8739769293e0f07c82215345
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-03-20 14:13:05 -0300

    main app structure

--- App.js
+++ App.js
@@ -1,21 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { quotes } from "./src/assets/quotes";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [image, setImage] = useState(
+		require("./src/assets/images/biscoito.png")
+	);
+	const [text, setText] = useState("Quebre o biscoito e encontre uma frase!");
+	const [status, setStatus] = useState(false);
+	function getRandomQuote() {
+		let random = Math.floor(Math.random() * quotes.length);
+		setImage(require("./src/assets/images/biscoitoAberto.png"));
+		setText(quotes[random]);
+		setStatus(!status);
+	}
+
+	function restartCookie() {
+		setImage(require("./src/assets/images/biscoito.png"));
+		setText("Quebre o biscoito e encontre uma frase!");
+		setStatus(!status);
+	}
+
+	return (
+		<View style={styles.container}>
+			<StatusBar style="auto" />
+			<Image source={image} style={styles.image} />
+			<Text style={styles.cookieText}>"{text}"</Text>
+
+			{status === false ? (
+				<TouchableOpacity onPress={getRandomQuote} style={styles.button}>
+					<View style={styles.buttonArea}>
+						<Text style={styles.buttonText}>Quebrar biscoito</Text>
+					</View>
+				</TouchableOpacity>
+			) : (
+				<TouchableOpacity
+					onPress={restartCookie}
+					style={[styles.button, { marginTop: 10, borderColor: "#000" }]}
+				>
+					<View style={styles.buttonArea}>
+						<Text style={[styles.buttonText, { color: "#000" }]}>
+							Reiniciar biscoito
+						</Text>
+					</View>
+				</TouchableOpacity>
+			)}
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	image: {
+		width: 250,
+		height: 250,
+	},
+	cookieText: {
+		fontSize: 20,
+		color: "orange",
+		margin: 30,
+		fontStyle: "italic",
+		textAlign: "center",
+	},
+	button: {
+		width: 250,
+		height: 50,
+		borderColor: "orange",
+		borderWidth: 2,
+		borderRadius: 20,
+	},
+	buttonArea: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+	},
+	buttonText: {
+		fontSize: 20,
+		color: "orange",
+		fontWeight: "bold",
+	},
 });
